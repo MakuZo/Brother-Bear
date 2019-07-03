2742f36d3a0fc4600772cccaea3a4beb5ce8b52c
MakuZo <MakuZo90@gmail.com>
2019-07-03 21:10:02 +0200

    Added docs for utils.get_short_id

--- core/utils.py
+++ core/utils.py
@@ -4,6 +4,7 @@ from core.models import ShortenedURL
 
 
 def get_short_id():
+    """Returns UNIQUE id for a ShortenedURL"""
     while True:
         id = get_random_string(length=8)
         if not ShortenedURL.objects.filter(id=id).exists():
