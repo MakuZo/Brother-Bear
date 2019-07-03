c8b884b3c2cfa1c972be6a25fd4c4136fda90a80
MakuZo <MakuZo90@gmail.com>
2019-07-03 21:08:43 +0200

    Filled README, added AdminModel for ShortenedURL

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from core.models import ShortenedURL
 
-admin.site.register(ShortenedURL)
+@admin.register(ShortenedURL)
+class ShortenedURLAdmin(admin.ModelAdmin):
+    fields = ('id', 'url')
+    list_display = ('id', 'url')
+
